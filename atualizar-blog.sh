a33e5b790db46bcc5d295461293ebe5e758062c8
Paulo Mainhard dos Santos <paulomainhard@Paulos-iMac.Home>
2025-07-19 02:24:11 +0100

    Atualizando blog automaticamente

--- atualizar-blog.sh
+++ atualizar-blog.sh
@@ -4,6 +4,9 @@
 # Caminho até a pasta do projeto
 cd "/Users/paulomainhard/Desktop/Pastas/Mainhard/GUT CARE SOLUTIONS/gut-care-blog"
 
+# Gerar sitemap atualizado
+./gerar-sitemap.sh
+
 # Atualizar o repositório Git
 git add .
 git commit -m "Atualizando blog automaticamente"
